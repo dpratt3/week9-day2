b1e1186cd784bc8e09836ff407de5ce2cc4cdff4
David Pratt <david@midnightmechanism.com>
2022-04-19 18:16:46 -0500

    Solve addManyNums10

--- phase-2.js
+++ phase-2.js
@@ -2,7 +2,6 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
   let sums = []
   for(let i = 1; i <= 10; i+= increment){
     sums.push(addNums(i))
@@ -12,7 +11,11 @@ function addNums10(increment) {
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+  let sums = []
+  for(let i = 1; i <= 10; i++){
+    sums.push(addManyNums(i * increment))
+  }
+  return sums;
 }
 
 module.exports = [addNums10, addManyNums10];
