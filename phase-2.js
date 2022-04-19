b96d5bd3444563c28fc52bbf94317abd503b9c9d
David Pratt <david@midnightmechanism.com>
2022-04-19 18:12:28 -0500

    Finish addNums10

--- phase-2.js
+++ phase-2.js
@@ -3,13 +3,16 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
-
+  let sums = []
+  for(let i = 1; i <= 10; i+= increment){
+    sums.push(addNums(i))
+  }
+  return sums;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
-
 }
 
 module.exports = [addNums10, addManyNums10];
