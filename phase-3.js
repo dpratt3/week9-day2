7d42e3478f5a0b0f90c6023849576af369cb2d1c
David Pratt <david@midnightmechanism.com>
2022-04-19 19:37:16 -0500

    Fix merge conflicts

--- phase-3.js
+++ phase-3.js
@@ -1,21 +1,20 @@
 const [addNums, addManyNums] = require("./phase-1");
 
-function addNums10(increment) {
+function addNums10Timing(increment) {
 
   let sums = []
   for (let i = 1; i <= 10; i++) {
     //console.time()
-    start = Date.now()
+    let start = Date.now()
     sums.push(addNums(i * increment))
-    end = Date.now()
+    let end = Date.now()
 
     console.log(end - start)
   }
   return sums;
 }
 
-//addNums10Timing(10**8)
-addNums10(10 ** 8)
+addNums10Timing(10**8)
 
 // Your code here
 
@@ -25,9 +24,9 @@ function addManyNums10Timing(increment) {
   // Your code here
   let sums = []
   for(let i = 1; i <= 15; i++){
-    start = Date.now()
+    let start = Date.now()
     sums.push(addManyNums(i * increment))
-    end = Date.now()
+    let end = Date.now()
     console.log(end - start)
   }
   return sums;
